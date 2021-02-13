4cdb9dd09e84821f0d8ca5f43ae815d8aef2bbf1
avellar1975 <evandro.avellar@gmail.com>
2021-02-13 10:03:44 -0300

    Testes para ver o CI rodando

---
+++ funcao.py
@@ -0,0 +1,10 @@
+"""Função para implementar um testeself.
+
+Recebe parm1 e parm2
+retorna resultado
+"""
+
+
+def funcao_para_testar(parm1, parm2):
+    """Retorna multiplicação de dois argumentos."""
+    return parm1 * parm2
---
+++ tests/function_test.py
@@ -0,0 +1,5 @@
+from funcao import funcao_para_testar
+
+
+def teste_basico():
+    assert funcao_para_testar(3, 5) == 15
