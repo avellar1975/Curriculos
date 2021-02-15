d3e2a4459450306cf15b219ed0cb1f95f0e9eaa6
avellar75 <evandro.avellar@gmail.com>
2021-02-15 15:14:28 -0300

    function_test

--- tests/function_test.py
+++ tests/function_test.py
@@ -3,3 +3,7 @@ from funcao import funcao_para_testar
 
 def teste_basico():
     assert funcao_para_testar(3, 5) == 15
+
+
+def teste_numeros_negativos():
+    assert funcao_para_testar(-3, 9) == -27
