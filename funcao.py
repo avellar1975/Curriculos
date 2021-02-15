dbb8aa7909fd6cafa22d5dfc778413ff014968dc
avellar75 <evandro.avellar@gmail.com>
2021-02-15 15:37:26 -0300

    nova funcao

--- funcao.py
+++ funcao.py
@@ -8,3 +8,6 @@ retorna resultado
 def funcao_para_testar(parm1, parm2):
     """Retorna multiplicação de dois argumentos."""
     return parm1 * parm2
+
+def funcao_nova(parm1, parm2, parm3):
+    return (parm1 + parm2 + parm3)/3
