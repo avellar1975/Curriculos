b4037fdf8aa04dd8027cf02f7ca2d88feb5114d6
avellar1975 <evandro.avellar@gmail.com>
2021-03-08 07:49:38 -0300

    Finalizada a parte de frontend

--- curriculos/base/views.py
+++ curriculos/base/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def consultar(request):
+    return render(request, 'base/consultar.html')
+
+
+def cadastrar(request):
+    return render(request, 'base/cadastrar.html')
+
+
+def alterar(request):
+    return render(request, 'base/alterar.html')
