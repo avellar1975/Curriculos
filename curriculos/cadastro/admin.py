a0174c7cf5eb22c541aa0c40a5d2e7492c93489b
avellar1975 <evandro.avellar@gmail.com>
2021-04-21 22:36:21 -0300

    Complemento cadastro

--- curriculos/cadastro/admin.py
+++ curriculos/cadastro/admin.py
@@ -1,8 +1,14 @@
 from django.contrib.admin import ModelAdmin, register
-from curriculos.cadastro.models import Pessoa
+from curriculos.cadastro.models import Pessoa, Formacao
 
 
 @register(Pessoa)
 class PessoaAdmin(ModelAdmin):
     list_display = ('nome', 'creation')
     ordering = ('nome',)
+
+
+@register(Formacao)
+class FormacaoAdmin(ModelAdmin):
+    list_display = ('cod', 'nome')
+    ordering = ('cod',)
--- curriculos/cadastro/models.py
+++ curriculos/cadastro/models.py
@@ -5,5 +5,19 @@ class Pessoa(models.Model):
     cpf = models.CharField(max_length=11)
     nome = models.CharField(max_length=60)
     data_nascimento = models.DateField()
-    creation = models.DateField(auto_now_add=True)
-    telefone = models.CharField(max_length=20, default="")
+    creation = models.DateTimeField(auto_now_add=True)
+    telefone = models.CharField(max_length=20, default="(00)000-000-000")
+    formacao = models.ForeignKey('Formacao',
+                                 on_delete=models.SET_NULL,
+                                 null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Formacao(models.Model):
+    cod = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
